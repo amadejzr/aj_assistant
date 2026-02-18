f9db8d35b7c446e3e606bc95038a4f22aeabbbe5
amadejzr <amadej@jerlah.si>
2026-02-18 09:54:01 +0100

    add logo export and update app logo

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -216,7 +216,7 @@ class _SplashScreenState extends State<SplashScreen>
                       child: Opacity(
                         opacity: _titleOpacity.value,
                         child: Text(
-                          'AJ Assistant',
+                          'AJ',
                           style: TextStyle(
                             fontFamily: 'CormorantGaramond',
                             fontSize: 26,
